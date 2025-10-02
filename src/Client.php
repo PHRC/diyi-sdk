795461067bea03733b27d177cfebf3b4204e0d5d
SprayHank <fantasyoui@gmail.com>
2025-10-02 15:02:49 +0800

    Implement address generation using Guzzle HTTP client

--- src/Client.php
+++ src/Client.php
@@ -5,8 +5,25 @@ declare( strict_types = 1 );
 namespace DiYi;
 
 
+use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Psr7\Request;
+
 class Client extends Api
 {
     public function generate_address()
-    {}
+    {
+        $client = new GuzzleHttpClient();
+        $options = [
+            'form_params' => [
+                'chain' => 'TRX',
+                'mch_no' => '',
+                'timestamp' => '',
+                'nonce' => '',
+                'sign' => '',
+            ],
+        ];
+        $request = new Request( 'POST', '/api/generate_address' );
+        $res = $client->sendAsync( $request, $options )->wait();
+        return $res->getBody();
+    }
 }
